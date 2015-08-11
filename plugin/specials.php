36f39813f50e76b838bf3c657de48310e843790f
Isha Thaker <ishathaker94@hotmail.com>
2015-08-11 16:49:20 -0400

    Forms

--- plugin/specials.php
+++ plugin/specials.php
@@ -73,3 +73,36 @@ function create_post_type() {
     )
   );
 }
+
+<html>
+<form method=post>
+	I want to see the special menu for: <select name="title">		/* This is the drop down menu for when the user wants to choose the operation they want */
+		<option>Select day:</option>
+		<option value="mon">Monday</option>
+		<option value="tues">Tuesday</option>
+		<option value="wed">Wednesday</option>
+		<option value="thurs">Thursday</option>
+		<option value="fri">Friday</option>
+		<option value="sat">Saturday</option>
+		<option value="sun">Sunday</option>
+	</select><br>
+	Number 1:<br>
+	<input type="text" name="number1">
+	<br>
+	Number 2:<br>
+	<input type="text" name="number2"><br>
+	
+	<input type="submit" value="Calculate Now">
+</form>
+</html>
+
+
+<?php 
+		echo $args['after_widget'];
+
+add_action( 'widgets_init', function(){
+     register_widget( 'Specials' );
+});
+
+?>
+
