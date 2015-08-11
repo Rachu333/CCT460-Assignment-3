60b157e769116e7a72976107d4bc8997937e8c8a
Isha Thaker <ishathaker94@hotmail.com>
2015-08-11 14:08:19 -0400

    Update specials.php

--- plugin/specials.php
+++ plugin/specials.php
@@ -19,29 +19,3 @@ Author URI: http://www.sheridanc.on.ca
  */
 
 
-function my_super_awesome_plugin(){ 
-	echo"Hey! My Super Awesome plugin works!";
-}
-add_action( 'wp_footer', 'my_super_awesome_plugin');
-
-function CD_Widget_Styles(){
-wp_enqueue_style('widget-style', plugins_url
-('/css/widget-style.css', __FILE__));
-}
-add_action( 'wp_enqueue_scripts', 'CD_Widget_Styles' );
-
-function super_awesome_link($content){
-	$content.='Check out my super awesome website at <a href="http://everythingisawesome.com">
-	http://everythingisawesome.com!</a>';
-	return	$content;
-	}
-	add_filter( 'the_content', 'super_awesome_link');
-
-function cd_check_WP_version(){
-	if ( version_compare( get_bloginfo( 'version' ), '4', '<' ) ){
-	wp_die( "You must update WordPress to use this plugin!" );   
-	}
-	}
-register_activation_hook( __FILE__,
-'cd_check_WP_version' );
-?>
