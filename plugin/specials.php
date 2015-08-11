2ad0e1088e7f569997540a6980196ac374be662d
Isha Thaker <ishathaker94@hotmail.com>
2015-08-11 14:03:11 -0400

    Update specials.php

--- plugin/specials.php
+++ plugin/specials.php
@@ -7,7 +7,7 @@
 /*
 Plugin Name: Specials
 Plugin URI: http://www.sheridanc.on.ca
-Description: The widget for El Burrito's specials
+Description: The widget for El Burrito's Specials
 Author: Isha Thaker, Alistair D'Cruz, Rachana Sai
 Version: 1.0
 Author URI: http://www.sheridanc.on.ca
