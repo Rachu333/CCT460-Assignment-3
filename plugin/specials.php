dc3095ef184280fb3b74872d79b202f7ff67242c
Rachu333 <rachana_sai@hotmail.com>
2015-08-12 01:07:27 -0400

    Enqueing style sheet!

--- plugin/specials.php
+++ plugin/specials.php
@@ -10,18 +10,21 @@ Author URI: phoenix.sheridanc.on.ca/~ccit2710
 */
 // Enqueing the style sheet into the plugin folder
 
+/*
 function register_plugin_styles()
 	{
  wp_register_style( 'plugin-style', plugins_url('/style.css' ) );
 	wp_enqueue_style( 'plugin-style' );
  }
 add_action( 'wp_enqueue_scripts', 'register_plugin_styles' );
+*/
+
 // FATIMA;S CODE
 
-function khashe_stylesheet() {
- wp_enqueue_style( 'style', plugins_url( '/khashewidget/style.css', __FILE__ ) );
+function specials_stylesheet() {
+ wp_enqueue_style( 'style', plugins_url( '/specials/style.css', __FILE__ ) );
  }
- add_action( 'template_redirect', 'khashe_stylesheet' );
+ add_action( 'template_redirect', 'specials_stylesheet' );
  
 function my_post_type() {
 	register_post_type( 'specials',
