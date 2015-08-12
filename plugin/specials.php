b7e96f7218900000995f9116ee708e06362d0d82
Isha Thaker <ishathaker94@hotmail.com>
2015-08-11 23:03:48 -0400

    Update specials.php

--- plugin/specials.php
+++ plugin/specials.php
@@ -1,108 +1,103 @@
 <?php
 
-/**
- * @package PHP Assignment
- * @version 1.0
- */
 /*
 Plugin Name: Specials
 Plugin URI: http://www.sheridanc.on.ca
 Description: The widget for El Burrito's specials
 Author: Isha Thaker, Alistair D'Cruz, Rachana Sai
 Version: 1.0
-Author URI: http://www.sheridanc.on.ca
+Author URI: phoenix.sheridanc.on.ca/~ccit2710
 */
 
-
-
-/**
- * Adds 'Specials' widget.
- */
-
-<<<<<<< HEAD
-class specials extends WP_Widget {
-           function Specials () {
-                     $widget_ops = array(
-                     'classname' => 'specials',
-                     'description' => 'our specials menu'
-           ); 
-
-          $this->WP_Widget(
-                     'specials',
-                     'specials',
-=======
-class Specials extends WP_Widget {
-           function Specials () {
-                     $widget_ops = array(
-                     'classname' => 'Specials',
-                     'description' => 'El Burrito Specials'
-           ); 
-
-          $this->WP_Widget(
-                     'Specials',
-                     'Specials',
->>>>>>> origin/master
-                     $widget_ops
-           );
- }
-
-          function widget($args, $instance) { // widget sidebar output
-                     extract($args, EXTR_SKIP);
-                     echo $before_widget; // pre-widget code from theme
-                      echo $after_widget; // post-widget code from theme
-           }
- }
-
-add_action(
-           'widgets_init',
-<<<<<<< HEAD
-           create_function('','return register_widget("specials");')
- );
-
-
- add_action( 'init', 'create_post_type' );
-function create_post_type() {
-  register_post_type( 'specials',
-    array(
-      'labels' => array(
-        'name' => __( 'Specials' ),
-        'singular_name' => __( 'Specials' )
-      ),
-      'public' => true,
-      'has_archive' => true,
-    )
-  );
+function my_post_type() {
+	register_post_type( 'specials',
+                array( 
+                     	'label' => __('Specials!'), 
+				'category_name' => 'specials',
+				'exclude_from_search' => true, // Exclude from Search Results
+				'capability_type' => 'page',
+				'public' => true, 
+				'show_ui' => true,
+				'show_in_nav_menus' => false,
+				'rewrite' => array(
+					'slug' => 'specials-menu',
+				),
+				'supports' => array(
+						'title',
+						'custom-fields',
+						'editor',
+            			'thumbnail')
+					) 
+				);
 }
+add_action('init', 'my_post_type');
+add_theme_support( 'post-thumbnails' );
 
-<html>
-<form method=post>
-	I want to see the special menu for: <select name="title">		/* This is the drop down menu for when the user wants to choose the operation they want */
-		<option>Select day:</option>
-		<option value="mon">Monday</option>
-		<option value="tues">Tuesday</option>
-		<option value="wed">Wednesday</option>
-		<option value="thurs">Thursday</option>
-		<option value="fri">Friday</option>
-		<option value="sat">Saturday</option>
-		<option value="sun">Sunday</option>
-	</select><br>
-	Number 1:<br>
-	<input type="text" name="number1">
-	<br>
-	Number 2:<br>
-	<input type="text" name="number2"><br>
-	
-	<input type="submit" value="Calculate Now">
-</form>
-</html>
+class Specials extends WP_Widget {
+	/**
+	 * This is to register widget with WordPress.
+	 */
+	function __construct() {
+		parent::__construct(
+			'specials',
+			__( 'Specials Plugin', 'text_domain' ), // This is the name of our widget
+			array( 'description' => __( 'The widget on the site', 'text_domain' ), ) // Args
+		);
+	}
 
-
-<?php 
+	/* This is the front-end display of the widget; how the widget will appear */
+	public function widget( $args, $instance ) {
+		echo $args['before_widget'];
+		if ( ! empty( $instance['title'] ) ) {
+			echo $args['before_title'] . apply_filters( 'widget_title', $instance['title'] ). $args['after_title'];
+		}
+		
+			$new = array('post_type'=> 'specials', 'showposts' => 5);
+			$my_query = new WP_query($new);
+		
+		if ($my_query->have_posts()) : while ($my_query->have_posts()) : $my_query->the_post(); ?>
+		
+		
+			<article id="post-<?php the_ID(); ?>"<?php post_class(); ?>>
+			<h2 class="post-title">
+				<?php the_title(); ?>
+			</h2>
+		<div class="entry-content">
+			<a href="<?php the_permalink(); ?>" title="<?php the_title_attribute(); ?>">
+		<?php the_post_thumbnail('thumbnail'); ?> </a> 	
+			
+		</div><!-- .entry-content -->
+		</article><!-- #post-## -->
+		<?php endwhile; endif; ?>
+<?php
 		echo $args['after_widget'];
+	}
+
+	/* This is the back-end of the widget form; how the widget will look. */
+	
+	public function form( $instance ) {
+		$title = ! empty( $instance['title'] ) ? $instance['title'] : __( 'New title', 'text_domain' );
+		?>
+		<p>
+		<label for="<?php echo $this->get_field_id( 'title' ); ?>"><?php _e( 'Title:' ); ?></label> 
+		<input class="widefat" id="<?php echo $this->get_field_id( 'title' ); ?>" name="<?php echo $this->get_field_name( 'title' ); ?>" type="text" value="<?php echo esc_attr( $title ); ?>">
+		</p>
+		<?php 
+	}
+
+	/**
+	 * 
+
+	 */
+	public function update( $new_instance, $old_instance ) {
+		$instance = array();
+		$instance['title'] = ( ! empty( $new_instance['title'] ) ) ? strip_tags( $new_instance['title'] ) : '';
+
+		return $instance;
+	}
+
+} // 
 
 add_action( 'widgets_init', function(){
-     register_widget( 'Specials' );
+	register_widget( 'Specials' );
 });
-
-?>
-
